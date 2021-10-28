94a253981fa65e927d32dfebd11ba1a57e8464f3
Hendrik Kueck <hendrik@pocketpixels.com>
2021-10-28 15:46:16 -0700

    Added additional augmentations to hopefully make the model more robust to different colors in camera image vs synthetic.

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -22,8 +22,12 @@ class Albumentations:
             self.transform = A.Compose([
                 A.Blur(p=0.1),
                 A.MedianBlur(p=0.1),
-                A.ToGray(p=0.05),
-                A.ISONoise(p=0.1)],
+                # A.ToGray(p=0.05),
+                A.ISONoise(p=0.5),
+                A.OpticalDistortion(p=0.5),
+                A.RandomToneCurve(p=0.5),
+                A.RGBShift(p=0.5)
+            ],
                 bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
 
             logging.info(colorstr('albumentations: ') + ', '.join(f'{x}' for x in self.transform.transforms))
